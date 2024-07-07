d4140d23eff4d1c2d4097d12af79b90eb2e0392b
Emmanuel Chaboud <42184090+emmcb@users.noreply.github.com>
2024-07-07 11:29:07 +0200

    Add _h float16 literal

--- lib/math/include/math/half.h
+++ lib/math/include/math/half.h
@@ -8,4 +8,12 @@ using half_t = _Float16;
 using half_t = float;
 #endif
 
+namespace literal {
+
+constexpr half_t operator""_h(long double val) {
+    return static_cast<half_t>(val);
+}
+
+} // namespace literal
+
 } // namespace cxximg
