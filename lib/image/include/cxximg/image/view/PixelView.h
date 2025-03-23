9c5d53aeda3b8b89c2e2683a436fc28f9ab6256c
Emmanuel Chaboud <42184090+emmcb@users.noreply.github.com>
2025-03-23 14:12:04 +0100

    Update copyright year

--- lib/image/include/cxximg/image/view/PixelView.h
+++ lib/image/include/cxximg/image/view/PixelView.h
@@ -1,4 +1,4 @@
-// Copyright 2023 Emmanuel Chaboud
+// Copyright 2023-2025 Emmanuel Chaboud
 //
 /// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
