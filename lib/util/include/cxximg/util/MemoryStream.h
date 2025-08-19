053ff2e8f90408bd0d710007c02398753169040d
Emmanuel Chaboud <42184090+emmcb@users.noreply.github.com>
2025-08-19 16:50:14 +0200

    Implement seekoff for VectorStreamBuf

--- lib/util/include/cxximg/util/MemoryStream.h
+++ lib/util/include/cxximg/util/MemoryStream.h
@@ -50,18 +50,56 @@ public:
     const std::vector<char>& vec() const { return mData; }
 
     std::streamsize xsputn(const char* s, std::streamsize count) override {
-        const std::size_t oldSize = mData.size();
-        const std::size_t newSize = oldSize + count;
+        const std::size_t currentPutPos = pptr() - pbase();
+        const std::size_t requiredSize = currentPutPos + count;
 
-        mData.resize(newSize);
+        if (requiredSize > mData.size()) {
+            mData.resize(requiredSize);
+        }
+
         char* dataPtr = mData.data();
+        memcpy(dataPtr + currentPutPos, s, count);
 
-        memcpy(dataPtr + oldSize, s, count);
-
-        setp(dataPtr, dataPtr + newSize);
-        pbump(static_cast<int>(newSize));
+        setp(dataPtr, dataPtr + mData.size());
+        pbump(static_cast<int>(requiredSize));
 
         return count;
+    }
+
+protected:
+    pos_type seekpos(pos_type pos, std::ios_base::openmode which) override {
+        return seekoff(pos, std::ios_base::beg, which);
+    }
+
+    pos_type seekoff(off_type off, std::ios_base::seekdir dir, std::ios_base::openmode which) override {
+        if (which != std::ios_base::out) {
+            return -1;
+        }
+
+        std::streamoff newOff = -1;
+
+        if (dir == std::ios_base::beg) {
+            newOff = off;
+        } else if (dir == std::ios_base::cur) {
+            newOff = (pptr() - pbase()) + off;
+        } else if (dir == std::ios_base::end) {
+            // end does not make sense as we don't know it yet
+        }
+
+        if (newOff < 0) {
+            return -1; // invalid position
+        }
+
+        const auto newSize = static_cast<std::size_t>(newOff);
+        if (newSize > mData.size()) {
+            mData.resize(newSize);
+        }
+
+        char* dataPtr = mData.data();
+        setp(dataPtr, dataPtr + mData.size());
+        pbump(static_cast<int>(newOff));
+
+        return newOff;
     }
 
 private:
